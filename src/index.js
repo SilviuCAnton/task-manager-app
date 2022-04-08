f4d3f7d3ec9a311f6190dd9d1365c2c677600091
SilviuCAnton <silviu.c.anton@gmail.com>
2022-04-08 18:44:00 +0300

    Created node backend api for task management

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+// const Task = require('./models/task')
+// const User = require('./models/user')
+
+// const main = async () => {
+//     // const task = await Task.findById('62504ae2fcb95dd0dddf0bf1')
+//     // await task.populate('owner')
+//     // console.log(task.owner)
+
+//     const user = await User.findById('62504a691716f21fa19613ee')
+//     await user.populate('tasks')
+//     console.log(user.tasks)
+// }
+
+// main()
