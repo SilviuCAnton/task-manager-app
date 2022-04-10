83e1edcd1fef39a07c1e83462008671b343dce24
SilviuCAnton <silviu.c.anton@gmail.com>
2022-04-10 19:16:20 +0300

    Implemented profile image upload

--- src/models/user.js
+++ src/models/user.js
@@ -42,14 +42,15 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
-    tokens: [
-        {
+    tokens: [{
             token: {
                 type: String,
                 required: true
             }
-        }
-    ]
+    }],
+    avatar: {
+        type: Buffer
+    }
 }, {
     timestamps: true,
 })
@@ -66,6 +67,7 @@ userSchema.methods.toJSON = function () {
 
     delete userObject.password
     delete userObject.tokens
+    delete userObject.avatar
 
     return userObject
 }
